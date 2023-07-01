3dcb3547fc1cb2c5c4df952b09a69da1e01ba7a4
Renato1478 <renatoperesdesouza@hotmail.com>
2023-07-01 12:15:23 -0300

    map focusing on rengion by searchbar

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import MapView from "react-native-maps";
 
 import styled from "styled-components";
 
 import Search from "../components/search.component";
+import { LocationContext } from "../../../services/location/location.context";
+import { RestaurantsContext } from "../../../services/restaurant/restaurant.context";
 
 const Map = styled(MapView)`
   height: 100%;
@@ -12,10 +14,38 @@ const Map = styled(MapView)`
 `;
 
 export const MapScreen = () => {
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantsContext);
+
+  const [latDelta, setLatDelta] = useState(0);
+
+  const { lat, lng, viewport } = location;
+
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const soutwestLat = viewport.southwest.lat;
+
+    const newLatDelta = northeastLat - soutwestLat;
+    setLatDelta(newLatDelta);
+  }, [location, viewport]);
+
   return (
     <>
       <Search />
-      <Map />
+      <Map
+        // coordinates
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.04,
+        }}
+      >
+        {restaurants.map((restaurant) => {
+          return null;
+          // return <MapView.Marker coordinate />;
+        })}
+      </Map>
     </>
   );
 };
